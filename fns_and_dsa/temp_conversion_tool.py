de6615332434185d4666674292811266bd8c143a
fkde17 <75939288+fkde17@users.noreply.github.com>
2025-09-28 23:30:34 +0300

    Fix temp conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,15 +1,15 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 9/5
-CELSIUS_TO_FAHRENHEIT_FACTOR = 5/9
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 temp = int(input("Enter the temperature to convert: "))
 temp_unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 
 def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
-    print (f"{temp}°C is {CELSIUS_TO_FAHRENHEIT_FACTOR * (celsius - 32)}°F")
+    print (f"{temp}°C is {CELSIUS_TO_FAHRENHEIT_FACTOR * celsius + 32}°F")
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
-    print (f"{temp}°F is  {FAHRENHEIT_TO_CELSIUS_FACTOR*fahrenheit + 32}°C")
+    print (f"{temp}°F is  {FAHRENHEIT_TO_CELSIUS_FACTOR*(fahrenheit - 32)}°C")
 
 if temp_unit == "C":
     convert_to_fahrenheit(temp)
