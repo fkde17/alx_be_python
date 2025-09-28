f7e2ad416789c2ecf00cfc343163aa1f0a9d9ba1
fkde17 <75939288+fkde17@users.noreply.github.com>
2025-09-28 22:30:04 +0300

    Add arithemetic operation

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+match operation:
+    case "add":
+        print(f"Result: {num1 + num2}")
+    case "subtract":
+        print(f"Result: {num1 - num2}")
+    case "multiply":
+        print(f"Result: {num1 * num2}")
+    case "division":
+        if num2 == 0:
+            print("Error: DIVISION BY ZERO")
+        else:
+            print(f"Result: {num1 + num2}")
+
+
