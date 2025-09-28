ad6202a5d238e10bf0a50684f106107c04cb8e44
fkde17 <75939288+fkde17@users.noreply.github.com>
2025-09-28 22:34:26 +0300

    Fix arithmetic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,7 +3,7 @@ num2 = float(input("Enter the second number: "))
 operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
 
-def perform_operation(num1,num2,operation):
+def perform_operation(num1, num2, operation):
 
     match operation:
         case "add":
